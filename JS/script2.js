833a1852293dfece34ae8312c8dd1427b45d61eb
fvince25 <fvince25@yahoo.fr>
2018-05-08 12:05:53 +0200

    Protection contenu non selectionable Flex Sim

--- JS/script2.js
+++ JS/script2.js
@@ -5,6 +5,7 @@ $(document).ready(
 
         var height_1 = 75;
         var width_1 = 100;
+        var Id_mem;
 
         var list_color = [
             '#851515',
@@ -35,6 +36,7 @@ $(document).ready(
 
         function check_position(event) {
 
+
             var h = event.target.offsetHeight;
             var w = event.target.offsetWidth;
 
@@ -44,113 +46,124 @@ $(document).ready(
             var ID = event.target.id;
 
 
-            if((y > h - 12 && y < h - 3))
+            if (event.target.id.length > 0)
             {
+                if ((y > h - 12 && y < h - 3)) {
 
-                $(document).mousemove(function(event) {
-                    $("#cadre > div").off('mousedown');
-                    $('#' + ID).css("cursor", "n-resize");
-                    $('#' + ID).append('<div class="temp_win"></div>');
+                    $(document).mousemove(function (event) {
+                        $("#cadre > div").off('mousedown');
+                        $('#' + ID).css("cursor", "n-resize");
+                        $('#' + ID).append('<div class="temp_win"></div>');
 
-                    var height_;
-                    if (event.offsetY > 75)
-                        height_ = event.offsetY;
-                    else
-                        height_ = 75;
+                        var height_;
+                        if (event.offsetY > 75)
+                            height_ = event.offsetY;
+                        else
+                            height_ = 75;
 
-                    $('#' + ID + ' > .temp_win').css("height",height_);
-                    $('#' + ID + ' > .temp_win').css("width",$('#' + ID).css("width"));
-
-
-                    $(document).mouseup(function() {
-                        $('#' + ID).css("cursor", "default");
-                        $(document).off('mousemove');
-                        $('#' + ID).css("height",height_);
-                        $('#' + ID + ' > .temp_win').remove();
-                        $("#cadre > div").off('mousedown').on('mousedown',function(event)
-                        {
-                            check_position(event);
-                        });
-                    })
-
-                })
+                        $('#' + ID + ' > .temp_win').css("height", height_);
+                        $('#' + ID + ' > .temp_win').css("width", $('#' + ID).css("width"));
 
 
-            }
-
-            if((x > w - 12 && x < w - 3))
-            {
-                $(document).mousemove(function(event) {
-
-                    var width_;
-                    if (event.offsetX > 100)
-                        width_ = event.offsetX;
-                    else
-                        width_ = 100;
-
-                    $("#cadre > div").off('mousedown');
-                    $('#' + ID).css("cursor", "e-resize");
-                    $('#' + ID).append('<div class="temp_win"></div>');
-                    $('#' + ID + ' > .temp_win').css("width",width_);
-                    $('#' + ID + ' > .temp_win').css("height",$('#' + ID).css("height"));
-
-
-                    $('#' + ID ).children('img').css('right', 10);
-                    $(document).mouseup(function() {
-                        $('#' + ID).css("cursor", "default");
-                        $(document).off('mousemove');
-                        $('#' + ID).children('img').css('right', 10);
-                        $('#' + ID + ' > .temp_win').remove();
-                        $('#' + ID).css("width",width_);
-                        $("#cadre > div").off('mousedown').on('mousedown',function(event)
-                        {
-                            check_position(event);
-                        });
+                        $(document).mouseup(function () {
+                            $('#' + ID).css("cursor", "default");
+                            $(document).off('mousemove');
+                            $('#' + ID).css("height", height_);
+                            $('#' + ID + ' > .temp_win').remove();
+                            $("#cadre > div").off('mousedown').on('mousedown', function (event) {
+                                check_position(event);
+                            });
+                        })
 
                     })
 
-                })
-            }
 
-            if((y > h - 12 && y < h - 3) && (x > w - 12 && x < w - 3))
-            {
-                $(document).mousemove(function(event) {
+                }
 
-                    $("#cadre > div").off('mousedown');
-                    var width_;
-                    if (event.offsetX > 100)
-                        width_ = event.offsetX;
-                    else
-                        width_ = 100;
+                if ((x > w - 12 && x < w - 3)) {
+                    $(document).mousemove(function (event) {
 
-                    var height_;
-                    if (event.offsetY > 75)
-                        height_ = event.offsetY;
-                    else
-                        height_ = 75;
+                        var width_;
+                        if (event.offsetX > 100)
+                            width_ = event.offsetX;
+                        else
+                            width_ = 100;
 
-                    $('#' + ID).append('<div class="temp_win"></div>');
-                    $('#' + ID + ' > .temp_win').css("width",width_);
-                    $('#' + ID + ' > .temp_win').css("height",height_);
-                    $('#' + ID).css("cursor", "nw-resize");
+                        $("#cadre > div").off('mousedown');
+                        $('#' + ID).css("cursor", "e-resize");
+                        $('#' + ID).append('<div class="temp_win"></div>');
+                        $('#' + ID + ' > .temp_win').css("width", width_);
+                        $('#' + ID + ' > .temp_win').css("height", $('#' + ID).css("height"));
 
-                    $('#' + ID).children('img').css('right', 10);
-                    $(document).mouseup(function() {
-                        $(document).off('mousemove');
-                        $('#' + ID).css("height",height_);
-                        $('#' + ID).css("width",width_);
-                        $('#' + ID + ' > .temp_win').remove();
-                        $('#' + ID).css("cursor", "default");
 
-                        $("#cadre > div").off('mousedown').on('mousedown',function(event)
-                        {
-                            check_position(event);
-                        });
+                        $(document).mouseup(function () {
+                            $('#' + ID).css("cursor", "default");
+                            $(document).off('mousemove');
+                            // $('#' + ID).children('i').css('right', 0);
+                            $('#' + ID + ' > .temp_win').remove();
+                            $('#' + ID).css("width", width_);
+                            $("#cadre > div").off('mousedown').on('mousedown', function (event) {
+                                check_position(event);
+                            });
+
+                        })
 
                     })
+                }
 
-                })
+                if ((y > h - 12 && y < h - 3) && (x > w - 12 && x < w - 3)) {
+                    $(document).mousemove(function (event) {
+
+                        $("#cadre > div").off('mousedown');
+                        var width_;
+                        if (event.offsetX > 100)
+                            width_ = event.offsetX;
+                        else
+                            width_ = 100;
+
+                        var height_;
+                        if (event.offsetY > 75)
+                            height_ = event.offsetY;
+                        else
+                            height_ = 75;
+
+                        $('#' + ID).append('<div class="temp_win"></div>');
+                        $('#' + ID + ' > .temp_win').css("width", width_);
+                        $('#' + ID + ' > .temp_win').css("height", height_);
+                        $('#' + ID).css("cursor", "nw-resize");
+
+
+                        $(document).mouseup(function () {
+                            // $('#' + ID).children('i').css('right', 0);
+                            $(document).off('mousemove');
+                            $('#' + ID).css("height", height_);
+                            $('#' + ID).css("width", width_);
+                            $('#' + ID + ' > .temp_win').remove();
+                            $('#' + ID).css("cursor", "default");
+
+                            $("#cadre > div").off('mousedown').on('mousedown', function (event) {
+                                check_position(event);
+                            });
+
+                        })
+
+                    })
+                }
             }
+
+        }
+
+        function valid_element() {
+            $('section input').each(function() {
+                if($(this).val().length > 0)
+                {
+                    var property = $(this).attr('id');
+                    var value_prop = $(this).val();
+                    $('#' + Id_mem).css(property , value_prop);
+                }
+            });
+            
+            $("#menu").remove();
 
         }
 
@@ -197,15 +210,46 @@ $(document).ready(
             last_value++;
 
             $('#cadre').append('<div class  ="noselect" unselectable="on" onselectstart="return false;" id="' + last_value + '">' + last_value +
-                '<img title = "supprimer" src="images/Delete.jpg"/></div>');
+                '<i title="Supprimer" class="fas fa-times"></i>' +
+                // '<i unselectable="off" onselectstart="" class="fas fa-bars select"></i>' +
+                '</div>');
 
             $('#' + last_value).css('height', height_1);
             $('#' + last_value).css('width', width_1);
             $('#' + last_value).css('background-color', list_color[modulo(last_value)]);
 
-            $("#cadre > div > img").on("click",function() {
+            $("#cadre > div > .fa-times").on("click",function() {
                 var Id_parent = $(this).parent().attr('id');
                 $("#" + Id_parent).remove();
+            });
+
+            $("#cadre > div > .fa-bars").on("click",function() {
+                Id_mem = $(this).parent().attr('id');
+                    $(this).append(
+                        '<div id="menu">' +
+                        '<section>' +
+                        '   <div>' +
+                        '       <h4>order : </h4>' +
+                        '       <input type="text" name="" id="order">' +
+                        '   </div>'    +
+                        '   <div>' +
+                        '       <h4>flex-grow : </h4>' +
+                        '       <input type="text" name="" id="flex-grow">' +
+                        '   </div>' +
+                        '   <div>' +
+                        '       <h4>flex-shrink : </h4>' +
+                        '       <input type="text" name="" id="flex-shrink"></div>' +
+                        '   <div>' +
+                        '       <h4>flex-basis : </h4>' +
+                        '       <input type="text" name="" id="flex-basis">' +
+                        '   </div>' +
+                        '   <div id="valid_elem">OK</div>' +
+                        '</section>' +
+                        '</div>'
+                );
+
+                $('#valid_elem').on('click',valid_element);
+                
             });
 
             $("#cadre > div").off('mousedown').on('mousedown',function(event)
@@ -245,10 +289,38 @@ $(document).ready(
         // Suppression d'un bloc
         //----------------------------------------------------------------
 
-        $("#cadre > div > img").on("click",function() {
+        $("#cadre > div > .fa-times").on("click",function() {
             var Id_parent = $(this).parent().attr('id');
             $("#" + Id_parent).remove();
-        })
+        });
+
+        $("#cadre > div > .fa-bars").on("click",function() {
+            Id_mem = $(this).parent().attr('id');
+            $(this).append(
+                '<div id="menu">' +
+                '<section>' +
+                '   <div>' +
+                '       <h4>order : </h4>' +
+                '       <input type="text" name="" id="order">' +
+                '   </div>'    +
+                '   <div>' +
+                '       <h4>flex-grow : </h4>' +
+                '       <input type="text" name="" id="flex-grow">' +
+                '   </div>' +
+                '   <div>' +
+                '       <h4>flex-shrink : </h4>' +
+                '       <input type="text" name="" id="flex-shrink"></div>' +
+                '   <div>' +
+                '       <h4>flex-basis : </h4>' +
+                '       <input type="text" name="" id="flex-basis">' +
+                '   </div>' +
+                '   <div id="valid_elem">OK</div>' +
+                '</section>' +
+                '</div>'
+            )
+        });
+
+        $('#valid_elem').on('click',valid_element);
 
 
     });
